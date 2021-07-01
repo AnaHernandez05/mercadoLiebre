359fd6dece4af92006341eb2298617956afb90b8
AnaHernandez05 <hernandezgonzalezg7@gmail.com>
2021-06-30 21:47:31 -0500

    Config

--- app.js
+++ app.js
@@ -12,4 +12,4 @@ app.get('/register',(req,res)=>{
 app.get('/login',(req,res)=>{
     res.sendFile(path.resolve(__dirname,'./views/login.html'));
 });
-app.listen(3000,()=> console.log('Servidor corriendo en el puerto 3000'));
+app.listen(process.env.PORT || 3000, function() {console.log('Servidor corriendo en el puerto 3000');});
